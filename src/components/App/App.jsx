e9da637d33eddf41f421e359abad999be92f98c2
Orlando Cabrera <orlando.osberto@gmail.com>
2021-04-09 12:56:19 -0600

    Colocando el nav por encima de todo y la pantalla de presentacion

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import Header from "./Header/Header"
+import Header from "./Header/Header";
 import Principal from "./Principal/Principal";
+import Info from "./Info/Info";
 
 export default function App() {
   return (
     <div className="row">
       <Header />
       <Principal />
+      <Info />
     </div>
   );
 }
--- src/components/App/Principal/Principal.jsx
+++ src/components/App/Principal/Principal.jsx
@@ -1,20 +1,9 @@
 import React from "react";
-import logo from "../../../imagenes/THMlogo.png";
 
 export default function Principal() {
   return (
     <div className="BackGround">
-      <container>
-        <img className="Logo" src={logo} alt="Logo" width="135" />
-        <container className="ingreso">
-          <button type="button">
-            Join Now
-          </button>
-          <button type="button">
-            Login
-          </button>
-        </container>
-      </container>
+      asd
     </div>
   );
 }
