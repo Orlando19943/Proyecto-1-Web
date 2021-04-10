cfb2e67ec6d98eb57d70e93f8a280675e66aa699
Orlando Cabrera <orlando.osberto@gmail.com>
2021-04-09 19:03:50 -0600

    Diseño inicial

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import logo from "../../../imagenes/THMlogo.png";
-// !asd
-// TODO
+
 export default function Principal() {
   return (
     <nav className="navbar navbar-expand-lg navbar-fondo">
       <img className="Logo" src={logo} alt="Logo" width="135" />
-      <container className="ingreso">
+      <div className="ingreso">
         <button type="button">
           Join Now
         </button>
         <button type="button">
           Login
         </button>
-      </container>
+      </div>
     </nav>
   );
 }
--- src/components/App/Info/Info.jsx
+++ src/components/App/Info/Info.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import logo from "../../../imagenes/badges.png";
 
 export default function Principal() {
   return (
     <div className="BackGroundInfo row">
-      asd
+      <img src={logo} alt="Logo" width="135" />
     </div>
   );
 }
