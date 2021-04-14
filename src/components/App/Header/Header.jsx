30e18d3a91cc7c7c618cd3e085909978d49c5c04
Orlando <orlando.osberto@gmail.com>
2021-04-13 18:29:28 -0600

    Terminando el nav

--- src/components/App/Header/Header.jsx
+++ src/components/App/Header/Header.jsx
@@ -7,16 +7,20 @@ export default function Principal() {
       <nav className="navbar navbar-expand-sm navbar-fondo">
         <div className="container-fluid">
           <img className="Logo" src={logo} alt="logo" />
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+          <button className="navbar-toggler" type=" " data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon" />
           </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02 botones">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ml-auto">
-              <li className="nav-item">
-                <div className="botones LogIn">Login</div>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
+            <ul className="navbar-nav mb-2 mb-lg-0 ml-auto botones">
+              <li className="nav-item LogIn">
+                <div className="a">Login </div>
               </li>
-              <li className="nav-item">
-                <div className="botones LogIn">Login</div>
+              <li className="nav-item joinNow">
+                <button className="tamaño">
+                  <div>
+                    JoinNow
+                  </div>
+                </button>
               </li>
             </ul>
           </div>
