8dcd47526a21b1c76aba10c1edd39a53a0e33de6
Orlando <orlando.osberto@gmail.com>
2021-04-16 02:26:26 -0600

    Colocando las cosas de la pantalla principal

--- src/components/App/Principal/Principal.jsx
+++ src/components/App/Principal/Principal.jsx
@@ -1,9 +1,37 @@
 import React from "react";
 import fondo from "../../../imagenes/waves.svg";
+import guides from "../../../imagenes/Principal/tryhackme_cloudguide.svg";
+import challenges from "../../../imagenes/Principal/tryhackme_cloudctf.svg";
 
 export default function Principal() {
   return (
     <div className="BackGround">
+      <div className="presentacion">
+        <div className="container">
+          <div className="todoEspacio">
+            <div className="row justify-content-center align-items-center">
+              <p className="tituloPrincipal">Cyber Security Training</p>
+              <p className="textoPrincipal">Making it easier to break into security, all through you browser.</p>
+            </div>
+            <div className="row justify-content-center align-items-center espaciado">
+              <div className="col-2">
+                <img className="imagen2" src={guides} alt="logo" width="auto" />
+              </div>
+              <div className="col-4">
+                <p className="subtitulo">Guides</p>
+                <p className="textoSecundario">Develop your skills by working through hands-on guided tasks and supporting videos.</p>
+              </div>
+              <div className="col-2">
+                <img className="imagen2" src={challenges} alt="logo" width="auto" />
+              </div>
+              <div className="col-4">
+                <p className="subtitulo">Challenges Training</p>
+                <p className="textoSecundario">Learn and test your skills with gamified, real-word cybersecurity challenges.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="imagenFondo">
         <img className="imagen" src={fondo} alt="logo" />
       </div>
